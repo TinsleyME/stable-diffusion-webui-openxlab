dbfadc6b6e355ca9042b81bcd91d0d1d1812a861
TinsleyME <93901876+TinsleyME@users.noreply.github.com>
2024-07-16 10:34:37 +0800

    Update Jupyter.py

--- test/Jupyter.py
+++ test/Jupyter.py
@@ -16,17 +16,6 @@ def is_port_in_use(port):
             return True
     return False
 
-def is_prime(num):
-    """
-    检查一个数是否为质数
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
 def start_jupyter_lab(port=8889):
     """
     启动 JupyterLab
@@ -45,16 +34,6 @@ def start_jupyter_lab(port=8889):
         jupyter_process = subprocess.Popen(jupyter_command, shell=True)
     else:
         print(f"Port {port} is already in use, JupyterLab cannot be started.")
-#         num_threads = threading.active_count()
-#         print("当前运行的线程数：%d" % num_threads)
-        #检查当前线程数小于 20 且为质数或者等于 2 的条件下，会执行等待时间以保持系统活力。
-#        if num_threads < 20 and (is_prime(num_threads) or num_threads == 2):
-#            scheduler = sched.scheduler(time.time, time.sleep)
-#            print(f"Start 6 hours : {time.ctime()}")
-#            scheduler.enter(6 * 60 * 60, 1, print, argument=(f"End 6 hours : {time.ctime()}",))# 维持系统活力,等待6小时
-#            scheduler.enter(12 * 60 * 60, 1, print, argument=(f"End 12 hours : {time.ctime()}",))# 维持系统活力,等待12小时
-#            scheduler.enter(24 * 60 * 60, 1, print, argument=(f"End 24 hours : {time.ctime()}",))# 维持系统活力,等待24小时
-#            scheduler.run()
 
 #多进程启动
 import multiprocessing
@@ -62,9 +41,3 @@ if __name__ == '__main__':
     ls = multiprocessing.Process(target=start_jupyter_lab) # 创建子进程
     ls.start() # 子进程 开始执行
     ls.join() # 等待子进程结束
-
-#多线程启动
-#if __name__ == '__main__':
-#    ls = threading.Thread(target=start_jupyter_lab) # 创建子线程
-#    ls.start() # 子线程 开始执行
-#    ls.join() # 等待子线程结束
